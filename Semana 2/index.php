a1bf63b246a6bd667dd5d909101807ae33f4843a
KrokZ09 <cramirezl2003@gmail.com>
2026-08-17 20:53:36 -0400

    Implementacion primeros archivos PHP

---
+++ Semana 2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pagina Principal</title>    
+    </head>
+    <body>
+        <h1>Pagina Principal</h1><br>
+        <a href="index.php">Ir a Principal</a><br>
+        <a href="empresa.php">Ir a Empresa</a><br>
+        <a href="productos.php">Ir a Productos</a><br>
+        <a href="servicios.php">Ir a Servicios</a><br>
+        <a href="contacto.php">Ir a Contacto</a><br>
+    </body>
+</html>
