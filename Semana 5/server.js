d772942ec8bbb5aa73b43a32a5bcb2fa38d599e9
KrokZ09 <cramirezl2003@gmail.com>
2026-09-08 01:01:25 -0300

    Terminada la implementacion del servicio conntraseña

--- Semana 5/server.js
+++ Semana 5/server.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const {ApolloServer, gql} = require('apollo-server-express');
-const Usuario = require('./models/usuario')
+const Usuario = require('./models/usuario');
+const usuario = require('./models/usuario');
 
 mongoose.connect('mongodb://localhost:27017/DesarrolloWebSemana5')
-const TypeDefs = gql`
+const typeDefs = gql`
     type Usuario{
         id: ID!
         nombre: String!
@@ -16,4 +17,70 @@ const TypeDefs = gql`
         nombre: String!
         pass: String!
     }
+    type Alert{
+        message: String
+    }
+    type Query{
+        getUsuarios: [Usuario]
+        getUsuariosById(id: ID!): Usuario
+    }
+    type Mutation{
+        addUsuario(input: UsuarioInput): Usuario
+        updUsuario(id: ID!, input: UsuarioInput): Usuario
+        delUsuario(id: ID!): Alert
+    }
 `;
+
+const resolvers = {
+    Query: {
+        async getUsuarios(obj){
+            const usuarios = await Usuario.find();
+            return usuarios;
+        },
+        async getUsuariosById(obj, {id}){
+            const usuarioBus = await Usuario.findById(id);
+            if (usuarioBus == null){
+                return null;
+            } else{
+                return usuarioBus;
+            }
+        }
+    },
+    Mutation: {
+        async addUsuario(obj, {input}){
+            const usuario = new Usuario(input);
+            await usuario.save();
+            return usuario;
+        },
+        async updUsuario(obj, {id, input}){
+            const usuario = await Usuario.findByIdAndUpdate(id, input);
+            return usuario; 
+        },
+        async delUsuario(obj, {id}){
+            await Usuario.deleteOne({_id: id});
+            return{
+                message: "Usuario Eliminado"
+            }
+        }
+    }
+};
+
+let apolloServer = null;
+const corsOption = {
+    origin: "http://localhost:8090",
+    credentials: false
+};
+
+async function startServer(){
+    apolloServer = new ApolloServer({typeDefs, resolvers, corsOption});
+    await apolloServer.start();
+    apolloServer.applyMiddleware({app, cors: false});
+}
+
+startServer();
+
+const app = express();
+app.use(cors());
+app.listen(8090, function(){
+    console.log("Graphql iniciado");
+});
